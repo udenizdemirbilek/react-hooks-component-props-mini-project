c86a97241ab1209124ec59c11de4d8441f548d30
demirbileku <udemirbilek2@gmail.com>
2021-06-12 22:01:01 +0300

    Done.

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,10 +1,19 @@
 import React from "react"
+// ☕️ 🍱
+function Article ({title, date="January 1, 1970", preview, minutes}) {
 
-function Article ({title, date="January 1, 1970", preview}) {
+function howLong (time){
+if (time > 10) {
+    return "🍱".repeat(Math.ceil(time/10))    
+} else {
+    return "☕️".repeat(Math.ceil(time/5))
+}
+}
+
     return (
         <article className="article-item">
         <h3>{title}</h3>
-        <small>{date}</small>
+        <small>{date}</small> . <small>{howLong(minutes)}</small>
         <p>{preview}</p>
         </article>
     )
--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -5,7 +5,7 @@ import Article from "./Article"
 function ArticleList ({posts}) {
     return (
         <main>{posts.map(post => {return (
-            <Article title={post.title} date={post.date} preview={post.preview} key={post.id}/>
+            <Article title={post.title} date={post.date} preview={post.preview} key={post.id} minutes={post.minutes}/>
         )})}
         </main>
     )
